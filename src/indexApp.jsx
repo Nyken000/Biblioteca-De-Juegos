09c1b7903aca5d0bcd34aa5239623c097fb9ed76
Nyken000 <javier.manriquez10@inacapmail.cl>
2024-07-22 19:18:26 -0400

    cambiando datos de index.htmal a indexApp.jsx

--- src/indexApp.jsx
+++ src/indexApp.jsx
@@ -1,52 +1,60 @@
-import { useEffect, useState } from "react"
-import { Formulario } from "./componentes/formApp"
-import { Tabla } from "./componentes/tableApp"
+import React, { useEffect, useState } from "react";
+import { Formulario } from "./componentes/formApp";
+import { Tabla } from "./componentes/tableApp";
+import { initNeonCursor } from './neonCursor';
 
 const inicial = () => {
-    return JSON.parse(localStorage.getItem('Biblioteca')) || []
+    return JSON.parse(localStorage.getItem('Biblioteca')) || [];
 }
 
 export const IndexApp = () => {
-    const [data, setData] = useState(inicial)
-    const [editItem, setEditItem] = useState(null)
+    const [data, setData] = useState(inicial);
+    const [editItem, setEditItem] = useState(null);
+
+    useEffect(() => {
+        initNeonCursor();
+    }, []);
 
     const addData = (valores) => {
         if (data.some(item => item.Id === valores.Id)) {
-            alert('El ID ya existe. Por favor, ingrese un ID único.')
-            return
+            alert('El ID ya existe. Por favor, ingrese un ID único.');
+            return;
         }
-        setData([...data, valores])
+        setData([...data, valores]);
     }
 
     const updateData = (valores) => {
-        const updatedData = data.map(item => item.Id === valores.Id ? valores : item)
-        setData(updatedData)
-        setEditItem(null)
+        const updatedData = data.map(item => item.Id === valores.Id ? valores : item);
+        setData(updatedData);
+        setEditItem(null);
     }
 
     useEffect(() => {
-        localStorage.setItem('Biblioteca', JSON.stringify(data))
-    }, [data])
+        localStorage.setItem('Biblioteca', JSON.stringify(data));
+    }, [data]);
 
     const deleteData = (valor) => {
-        setData(valor)
+        setData(valor);
     }
 
     return (
         <>
-            <div className="bg-success p-5 text-center text-white">
-                <h1>Datos</h1>
-            </div>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="mt-3 col-4">
-                        <Formulario addNew={addData} updateData={updateData} editItem={editItem} />
-                    </div>
-                    <div className="mt-3 col-8">
-                        <Tabla info={data} delData={deleteData} setEditItem={setEditItem} />
+            <div id="app"></div>
+            <div id="main-content">
+                <div className="bg-success p-5 text-center text-white">
+                    <h1>Datos</h1>
+                </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="mt-3 col-4">
+                            <Formulario addNew={addData} updateData={updateData} editItem={editItem} />
+                        </div>
+                        <div className="mt-3 col-8">
+                            <Tabla info={data} delData={deleteData} setEditItem={setEditItem} />
+                        </div>
                     </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
